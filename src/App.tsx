cf9d840b8ef618334e35c2b50a5f19bd90949d84
sergnio <sergnioa@gmail.com>
2026-02-20 16:51:28 -0600

    great success

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ const recommended = ["pikachu", "bulbasaur", "charmander", "squirtle"];
 const App = () => (
   <div className="flex flex-col gap-4 w-screen h-screen bg-amber-100 border rounded-md border-amber-700 text-black p-4 items-center justify-center">
     <div className="flex flex-col gap-2">
-      <h1 className="text-4xl font-bold">Hello from itsuki!</h1>
+      <h1 className="text-4xl font-bold">Mr Sir</h1>
       <h2 className="font-semibold">My Favorite Pokemons</h2>
       <div className="antialiased relative w-full flex flex-row gap-2">
         {recommended.map((pokemon) => (
